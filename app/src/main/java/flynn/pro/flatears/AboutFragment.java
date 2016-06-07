587fe71f1560b13dc20abc2fbff5ce0d9f2d94af
clackx <clackxs@gmail.com>
2016-06-07 16:49:06 +0300

    FIRST. JUST UI

---
+++ app/src/main/java/flynn/pro/flatears/AboutFragment.java
@@ -0,0 +1,34 @@
+package flynn.pro.flatears;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutFragment extends Fragment {
+
+    public static AboutFragment newInstance() {
+        AboutFragment fragment = new AboutFragment();
+        return fragment;
+    }
+
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+}
---
+++ app/src/main/java/flynn/pro/flatears/UploadFragment.java
@@ -0,0 +1,34 @@
+package flynn.pro.flatears;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UploadFragment extends Fragment {
+
+    public static UploadFragment newInstance() {
+        UploadFragment fragment = new UploadFragment();
+        return fragment;
+    }
+
+
+    public UploadFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upload, container, false);
+    }
+
+}
